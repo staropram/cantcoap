a8d84b24ace874a381aeea662b681722a1f6c232
Ashley Mills <ashley@ashleymills.com>
2013-10-08 14:59:51 +0100

    Updated.

--- search/all_73.js
+++ search/all_73.js
@@ -4,6 +4,7 @@ var searchData=
   ['setcontentformat',['setContentFormat',['../classCoapPDU.html#afc7d3485afa5f4c84f55a41562305d0f',1,'CoapPDU']]],
   ['setmessageid',['setMessageID',['../classCoapPDU.html#afd64c65662e72058abe6008a1abe08ec',1,'CoapPDU']]],
   ['setpayload',['setPayload',['../classCoapPDU.html#a68d5b39701189a853eb5403a1a1a77e0',1,'CoapPDU']]],
+  ['setpdulength',['setPDULength',['../classCoapPDU.html#a1fb6272cbc6db25f00a86c496f32cc85',1,'CoapPDU']]],
   ['settoken',['setToken',['../classCoapPDU.html#adafa3fa099f4ba5248a05bb4a120bbed',1,'CoapPDU']]],
   ['settokenlength',['setTokenLength',['../classCoapPDU.html#a371af095ec9b05194212abe4d3df5e6d',1,'CoapPDU']]],
   ['settype',['setType',['../classCoapPDU.html#a5aed3e2860acdd3beeac6a812fcf4700',1,'CoapPDU']]],
